eaf521c32166c33968739e034b7aa5be41ebbd0a
Anand Tripathi <shadarsh23@gmail.com>
2022-06-21 20:40:52 +0530

    completed project

---
+++ README.md
@@ -0,0 +1,2 @@
+# magic-memory
+Fun memory game made with react.
---
+++ src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css'
+function SingleCard({ card, handleChoice, flipped, disabled }) {
+
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card)
+        }
+    }
+
+    return (
+        <div className="card">
+            <div className={flipped ? "flipped" : ""}>
+                <img src={card.src} className="front" alt="card front" />
+                <img
+                    src="/img/cover4.jpeg"
+                    className="back"
+                    alt="card back"
+                    onClick={handleClick} />
+            </div>
+        </div>
+    )
+}
+
+export default SingleCard
